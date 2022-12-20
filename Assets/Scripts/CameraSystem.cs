c8bbdd4ca6fa95d45806b5c2bf7dbdf7656d390f
alex-rubickanov <alexander850m@gmail.com>
2022-12-20 18:23:45 +0400

    Fixed buttons

--- Assets/Scripts/CameraSystem.cs
+++ Assets/Scripts/CameraSystem.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class CameraSystem : MonoBehaviour
 {
     [SerializeField] GameObject[] CameraPositions;
+    [SerializeField] GameObject[] buttons;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow)) // camera 0
         {
             CameraPositions[0].SetActive(true);
             CameraPositions[1].SetActive(false);
@@ -16,23 +17,25 @@ public class CameraSystem : MonoBehaviour
             CameraPositions[3].SetActive(false);
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) // camera 2
         {
             CameraPositions[0].SetActive(false);
             CameraPositions[1].SetActive(false);
             CameraPositions[2].SetActive(true);
             CameraPositions[3].SetActive(false);
+            buttons[1].SetActive(true);
         }
 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.DownArrow)) // camera 1
         {
             CameraPositions[0].SetActive(false);
             CameraPositions[1].SetActive(true);
             CameraPositions[2].SetActive(false);
             CameraPositions[3].SetActive(false);
+            buttons[0].SetActive(true);
         }
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow)) //camera 3
         {
             CameraPositions[0].SetActive(false);
             CameraPositions[1].SetActive(false);
